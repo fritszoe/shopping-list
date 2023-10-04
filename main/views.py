c804f21e3788f88a788ac2a43c2447c914683786
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2023-10-04 13:32:45 +0700

    done tutorial-5

--- main/views.py
+++ main/views.py
@@ -3,13 +3,14 @@ from main.forms import ProductForm
 from django.urls import reverse
 from django.shortcuts import render
 from main.models import Product
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 import datetime
 # Create your views here.
 @login_required(login_url='/login')
@@ -88,6 +89,10 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -108,3 +113,19 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+...
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
